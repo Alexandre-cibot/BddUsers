44ad292c886cfdf89438c365585c011e251cb690
Ikimasoft <yann@ikimasoft.fr>
2016-04-17 18:00:09 +0200

    Suppression des indexes de la table utilisateur de demo eclatement

--- utilisateur.sql
+++ utilisateur.sql
@@ -28,10 +28,7 @@ CREATE TABLE `utilisateur` (
   `nom` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
   `type` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `prenom` (`prenom`,`nom`),
-  UNIQUE KEY `email` (`email`),
-  KEY `id_type` (`type`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9989 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -54,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-17 17:45:57
+-- Dump completed on 2016-04-17 17:59:42
